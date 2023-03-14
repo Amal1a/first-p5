91d155e4e927510775c645d522e72d631924e3a7
Amalia S <amalia.svejgaard@gmail.com>
2023-03-14 17:05:45 +0100

    dots full screen yay

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ function setup() {
   //createCanvas(windowWidth, windowHeight);
 
   //Inside a parent element
-  const canvas = createCanvas(700, 700);
+  // const canvas = createCanvas(700, 700);
+  const canvas = createCanvas(1512, 982);
   canvas.parent("sketch-holder");
 
   //basic setup
@@ -21,8 +22,8 @@ function draw() {
   //clear();
   // fill("blue");
   // square(300, 300, 200);
-  // fill(getRandInt(255), getRandInt(255), getRandInt(255));
-  // circle(getRandInt(width), getRandInt(height), getRandInt(150));
+  fill(getRandInt(255), getRandInt(255), getRandInt(255));
+  circle(getRandInt(width), getRandInt(height), getRandInt(150));
 }
 function mouseDragged() {
   //mouseX
@@ -40,54 +41,51 @@ function mouseClicked() {
 }
 
 /* ------ */
-let x = 0;
-let y = 100;
-let yModifier = 5;
-let xModifier = 5;
+// let x = 0;
+// let y = 100;
+// let yModifier = 5;
+// let xModifier = 5;
 
-function draw() {
-  //clear();
-  x += 5;
-  y += 5;
-  if (y + 100 > height) {
-    //y-=5;
-    yModifier = yModifier * -1;
-  }
-  if (y + 100 > width) {
-    //y-=5;
-    xModifier = xModifier * -1;
-  }
-
-  if (y < 0) {
-    //y-=5;
-    yModifier = yModifier * -1;
-  }
-  if (x < 0) {
-    //y-=5;
-    xModifier = xModifier * -1;
-  }
-  console.log(y, height);
-
-  fill("hotpink");
-  square(x, y, 100);
-
-  // if (keyIsDown(UP_ARROW)) {
-  //   //
-  //   y = y - 5;
-  // }
-  // if (keyIsDown(DOWN_ARROW)) {
-  //   //
-  //   y = y + 5;
-  // }
-  // if (keyIsDown(LEFT_ARROW)) {
-  //   //
-  //   y = x - 5;
-  // }
-  // if (keyIsDown(RIGHT_ARROW)) {
-  //   //
-  //   y = x + 5;
-  // }
-}
+//function draw() {
+// //clear();
+// x += 5;
+// y += 5;
+// if (y + 100 > height) {
+//   //y-=5;
+//   yModifier = yModifier * -1;
+// }
+// if (y + 100 > width) {
+//   //y-=5;
+//   xModifier = xModifier * -1;
+// }
+// if (y < 0) {
+//   //y-=5;
+//   yModifier = yModifier * -1;
+// }
+// if (x < 0) {
+//   //y-=5;
+//   xModifier = xModifier * -1;
+// }
+// console.log(y, height);
+// fill("hotpink");
+// square(x, y, 100);
+// if (keyIsDown(UP_ARROW)) {
+//   //
+//   y = y - 5;
+// }
+// if (keyIsDown(DOWN_ARROW)) {
+//   //
+//   y = y + 5;
+// }
+// if (keyIsDown(LEFT_ARROW)) {
+//   //
+//   y = x - 5;
+// }
+// if (keyIsDown(RIGHT_ARROW)) {
+//   //
+//   y = x + 5;
+// }
+//}
 
 // function keyPressed() {
 //   if (keyIsDown(UP_ARROW)) {
